e8513c2b6199c6035bbf380986e83b8d23dc1af4
MGasowski <gasowski0marcin@gmail.com>
2024-10-06 10:24:58 +0200

    drizzle config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/3IJ7NdTBtYbj90cf3V7jvfJRK2MLDOs4pBQbk6PE8Sm7XWuV",
@@ -12,10 +13,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
